bf24761c599b9f3c8316316c09ef65d88ca27051
Mayank-Sankhwar <mayank372sankhwar@gmail.com>
2025-10-03 04:11:44 +0530

    errorrrr

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
