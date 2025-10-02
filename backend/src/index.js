9b03a1ea5b7a339bbea9aef54b0cc8a36b355d9d
Mayank-Sankhwar <mayank372sankhwar@gmail.com>
2025-10-03 04:22:36 +0530

    errorrrr

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatitout-2j3o.onrender.com",
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
